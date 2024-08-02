9a32fa77efd8e5ec5b46700e4061cd474542b2be
HashirAKB <ahmedhashir96@gmail.com>
2024-08-02 22:22:25 +0530

    Subscribe component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
 import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -30,6 +31,7 @@ const App = () => {
       <Products />
       <TopProducts handleOrderPopup={handleOrderPopup}/>
       <Banner/>
+      <Subscribe/>
     </div>
   )
 };
