6abeca260a128e50b91ac4d548fb1197271403ad
HashirAKB <ahmedhashir96@gmail.com>
2024-08-02 22:08:39 +0530

    Completed Products Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -22,8 +23,9 @@ const App = () => {
 
   return(
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
-    <Navbar handleOrderPopup={handleOrderPopup}/>
-    <Hero handleOrderPopup={handleOrderPopup}/>
+      <Navbar handleOrderPopup={handleOrderPopup}/>
+      <Hero handleOrderPopup={handleOrderPopup}/>
+      <Products />
     </div>
   )
 };
